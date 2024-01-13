b1ece7e2f23a5834d25ae3627d46229c8cb78092
Manqoba-123 <mlangenimanqoba@gmail.com>
2024-01-13 02:18:45 +0200

    Frame1 completed

--- src/frames/FrameOne.jsx
+++ src/frames/FrameOne.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function FrameOne() {
   return (
-    <div>FrameOne</div>
+    <div className='bg-slate-500 w-screen h-screen
+    flex flex-row justify-center'>
+      <div className='bg-white w-1/4 h-2/4'>FrameOne</div>
+    </div>
   )
 }
